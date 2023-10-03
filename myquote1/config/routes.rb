ad8d1ef791041ae3b4d44e01688bc2321dd5844b
kaylebaker <student@lab000000.lkeiewvqx0kedh1jh5xmt2eoaf.px.internal.cloudapp.net>
2023-10-03 11:53:07 +0800

    Login form, sessions controller, routes

--- myquote1/config/routes.rb
+++ myquote1/config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
   get '/about', to: 'about#index'
+
+  # User authentication sessions
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
