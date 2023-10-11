7bc7752d17dc8162ec56e91cba70a1957bce66e2
kaylebaker <student@lab000000.lkeiewvqx0kedh1jh5xmt2eoaf.px.internal.cloudapp.net>
2023-10-11 18:58:31 +0800

    Added comments to source code

--- myquote1/app/controllers/search_controller.rb
+++ myquote1/app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def index
     category_query = params[:category_query]
     if category_query.present?
+      # Perform a database query to find all QuoteLists associated with a Category that matches category_query
       @categorymatch = QuoteList.joins(:category, :quote).where("categories.name LIKE ?", "%#{category_query}%")
     end
   end
